b3c80af6395e1647ce08f0b2b2332906621b97f0
KutselaDanylo <160267047+KutselaDanylo@users.noreply.github.com>
2024-12-25 18:35:21 +0100

    addit 3 homework

--- homework2.js
+++ homework2.js
@@ -1,47 +1,47 @@
-function celsiusToFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
-}
+// function celsiusToFahrenheit(celsius) {
+//     return (celsius * 9 / 5) + 32;
+// }
 
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
-}
+// function fahrenheitToCelsius(fahrenheit) {
+//     return (fahrenheit - 32) * 5 / 9;
+// }
 
-let celsiusValue = 25; 
-let fahrenheitValue = celsiusToFahrenheit(celsiusValue); 
-console.log(`${celsiusValue}°C is ${fahrenheitValue}°F`);
+// let celsiusValue = 25; 
+// let fahrenheitValue = celsiusToFahrenheit(celsiusValue); 
+// console.log(`${celsiusValue}°C is ${fahrenheitValue}°F`);
 
-fahrenheitValue = 77; 
-celsiusValue = fahrenheitToCelsius(fahrenheitValue);
-console.log(`${fahrenheitValue}°F is ${celsiusValue}°C`);
+// fahrenheitValue = 77; 
+// celsiusValue = fahrenheitToCelsius(fahrenheitValue);
+// console.log(`${fahrenheitValue}°F is ${celsiusValue}°C`);
 
-const daysInMonths = (30);
-const hoursInMonths = (daysInMonths * 24);
-const minitsInMonths = (hoursInMonths * 60);
-console.log(daysInMonths);
-console.log(hoursInMonths);
-console.log(minitsInMonths);
+// const daysInMonths = (30);
+// const hoursInMonths = (daysInMonths * 24);
+// const minitsInMonths = (hoursInMonths * 60);
+// console.log(daysInMonths);
+// console.log(hoursInMonths);
+// console.log(minitsInMonths);
 
-const helth =(100);
-const energy =(100);
-console.log(helth - 30);
-console.log((energy - 90));
+// const helth =(100);
+// const energy =(100);
+// console.log(helth - 30);
+// console.log((energy - 90));
 
-const totalPrice =(50);
-const discountetPrice = (totalPrice * 0.9);
-console.log(discountetPrice);
+// const totalPrice =(50);
+// const discountetPrice = (totalPrice * 0.9);
+// console.log(discountetPrice);
 
-const floutNumber = (12.78);  
-const roundetDown =(Math.floor(floutNumber));
-console.log(roundetDown);
+// const floutNumber = (12.78);  
+// const roundetDown =(Math.floor(floutNumber));
+// console.log(roundetDown);
 
-const floteString =(45.67);
-const parsedFloat =(Number.parseFloat(floteString));
-console.log(parsedFloat);
+// const floteString =(45.67);
+// const parsedFloat =(Number.parseFloat(floteString));
+// console.log(parsedFloat);
 
-const intString =(123);
-const parsendInt =(Number.parseInt(intString));
-console.log(parsendInt);
+// const intString =(123);
+// const parsendInt =(Number.parseInt(intString));
+// console.log(parsendInt);
 
-const number =(Math.sqrt(4));
-const sqrtNumber =(number);
-console.log(sqrtNumber);
+// const number =(Math.sqrt(4));
+// const sqrtNumber =(number);
+// console.log(sqrtNumber);
