2c93255c5ae3d6067c0125c64a6079fde036c901
KutselaDanylo <160267047+KutselaDanylo@users.noreply.github.com>
2025-03-27 16:59:36 +0100

    addit homework5

---
+++ homework4.js
@@ -0,0 +1,44 @@
+// document.getElementById("happy").onclick = function(){
+//      let firstValue = document.getElementById("inputb").value;
+//      let secondValue = document.getElementById("inputa").value;
+//      if (firstValue && secondValue){
+//         alert("fealds filld");
+//      }
+//      else{
+//         alert("fealds are not filld");
+//      }
+// }
+// document.getElementById("check").onclick = function(){
+//     let first = document.getElementById("first-number").value;
+//     let second = document.getElementById("second-number").value;
+//     let sum = (first + second);
+//     if (sum > 10){
+//        alert("sum bigger than 10");
+//     }
+//     else{
+//        alert("sum smaler than 10");
+//     }
+// }
+
+// let text = prompt("enter your text");
+// let button = confirm("перевіряти");
+// if(text.includes("JavaScript")){
+//   alert("Текст містить слово JavaScrip")
+// }
+// else{
+//   alert("Текст не містить слово JavaScript")
+// }
+
+// document.getElementById("check-data").onclick = function(){
+//     let first = document.getElementById("first-input").value;
+//     let second = document.getElementById("second-input").value;
+//     let third = document.getElementById("third-input").value;
+//     let sum = (first + second);
+//     if (first.lenght>3 && second.includes("@") && third.lenght>6){
+//        alert("go to other wegsite");
+//        window.location.href = "second.html";
+//     }
+//     else{
+//        alert("wrong answers");
+//     }
+// }
---
+++ homework5.js
@@ -0,0 +1,41 @@
+let text1 = "Hello";
+let text2 = "World";
+
+if (text1 !== "" && text2 !== "") {
+    console.log("Обидва поля заповнені");
+} else {
+    console.log("Не всі поля заповнені");
+}
+
+// 2. Обчислення суми чисел
+let num1 = 5;
+let num2 = 7;
+let sum = num1 + num2;
+
+if (sum > 10) {
+    console.log("Сума більша за 10");
+} else {
+    console.log("Сума менша або дорівнює 10");
+}
+let text = "Я вчу JavaScript";
+
+if (text.includes("JavaScript")) {
+    console.log("Текст містить слово JavaScript");
+} else {
+    console.log("Текст не містить слово JavaScript");
+}
+let number = 15;
+
+if (number > 10 && number < 20) {
+    console.log("Число входить в діапазон від 10 до 20")
+} else {
+    console.log("Число не входить в діапазон від 10 до 20");
+}
+let name = "Max";
+let email = "max@example.com";
+let password = "mypassword";
+if (name.length >= 3 && email.includes("@") && email.includes(".") && password.length >= 6) {
+    console.log("Перенаправлення на іншу сторінку");
+} else {
+    console.log("Помилка: неправильне заповнення")
+}
