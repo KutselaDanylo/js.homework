5d0007615e5240144d51a1cf20f32b3d59ccfe76
KutselaDanylo <160267047+KutselaDanylo@users.noreply.github.com>
2025-04-20 16:42:21 +0200

    addit homework

--- homework4.js
+++ homework4.js
@@ -1,44 +1,41 @@
-// document.getElementById("happy").onclick = function(){
-//      let firstValue = document.getElementById("inputb").value;
-//      let secondValue = document.getElementById("inputa").value;
-//      if (firstValue && secondValue){
-//         alert("fealds filld");
-//      }
-//      else{
-//         alert("fealds are not filld");
-//      }
-// }
-// document.getElementById("check").onclick = function(){
-//     let first = document.getElementById("first-number").value;
-//     let second = document.getElementById("second-number").value;
-//     let sum = (first + second);
-//     if (sum > 10){
-//        alert("sum bigger than 10");
-//     }
-//     else{
-//        alert("sum smaler than 10");
-//     }
+// let text1 = "Hello";
+// let text2 = "World";
+
+// if (text1 !== "" && text2 !== "") {
+//     console.log("Обидва поля заповнені");
+// } else {
+//     console.log("Не всі поля заповнені");
 // }
 
-// let text = prompt("enter your text");
-// let button = confirm("перевіряти");
-// if(text.includes("JavaScript")){
-//   alert("Текст містить слово JavaScrip")
-// }
-// else{
-//   alert("Текст не містить слово JavaScript")
-// }
+// // 2. Обчислення суми чисел
+// let num1 = 5;
+// let num2 = 7;
+// let sum = num1 + num2;
 
-// document.getElementById("check-data").onclick = function(){
-//     let first = document.getElementById("first-input").value;
-//     let second = document.getElementById("second-input").value;
-//     let third = document.getElementById("third-input").value;
-//     let sum = (first + second);
-//     if (first.lenght>3 && second.includes("@") && third.lenght>6){
-//        alert("go to other wegsite");
-//        window.location.href = "second.html";
-//     }
-//     else{
-//        alert("wrong answers");
-//     }
+// if (sum > 10) {
+//     console.log("Сума більша за 10");
+// } else {
+//     console.log("Сума менша або дорівнює 10");
+// }
+// let text = "Я вчу JavaScript";
+
+// if (text.includes("JavaScript")) {
+//     console.log("Текст містить слово JavaScript");
+// } else {
+//     console.log("Текст не містить слово JavaScript");
+// }
+// let number = 15;
+
+// if (number > 10 && number < 20) {
+//     console.log("Число входить в діапазон від 10 до 20")
+// } else {
+//     console.log("Число не входить в діапазон від 10 до 20");
+// }
+// let name = "Max";
+// let email = "max@example.com";
+// let password = "mypassword";
+// if (name.length >= 3 && email.includes("@") && email.includes(".") && password.length >= 6) {
+//     console.log("Перенаправлення на іншу сторінку");
+// } else {
+//     console.log("Помилка: неправильне заповнення")
 // }
---
+++ homework7.js
@@ -0,0 +1,18 @@
+
+let nums1 = [2, 5, 7];
+nums1[1] = 10;
+console.log(nums1);
+let strs1 = ["яблуко", "банан", "груша"];
+strs1.push("слива");
+console.log(strs1);
+let nums2 = [4, 7, 1, 3, 9];
+let sum = nums2.reduce((a, b) => a + b, 0);
+console.log(sum);
+let nums3 = [10, 20, 30, 40, 50];
+nums3.forEach(n => console.log(n));
+let strs2 = ["привіт", "кіт", "добрий день", "світло", "машина"];
+strs2.forEach(s => s.length > 5 && console.log(s))
+let nums4 = [3, 18, 7, 22, 5, 1, 14, 30, 6, 10];
+console.log(Math.max(...nums4))
+let nums5 = [4, 7, 12, 19, 8, 3, 22, 15, 6, 10]
+nums5.forEach(n => n % 2 === 0 && console.log(n))
