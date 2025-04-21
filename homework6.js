f60281d8f5ff2438a213af2994910a853559a97f
KutselaDanylo <160267047+KutselaDanylo@users.noreply.github.com>
2025-04-21 10:51:21 +0200

    addit homework8

--- homework6.js
+++ homework6.js
@@ -1,30 +1,30 @@
-let i = 1;
-while (i <= 10) {
-  console.log(i);
-  i++;
-}
-for (let j = 2; j <= 20; j++) {
-  if (j % 2 === 0) {
-    continue; 
-  }
-  console.log(j); 
-}
-for (let k = 1; k <= 10; k++) {
-  console.log(`7 x ${k} = ${7 * k}`);
-}
-let n = 12;
-for (let m = 1; ; m++) {
-  if (m >= n) {
-    break;
-  }
-  console.log(m);
-}
-let x = 1;
-while (x <= 20) {
-  if (x % 3 === 0) {
-    x++;
-    continue;
-  }
-  console.log(x);
-  x++;
-}
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// for (let j = 2; j <= 20; j++) {
+//   if (j % 2 === 0) {
+//     continue; 
+//   }
+//   console.log(j); 
+// }
+// for (let k = 1; k <= 10; k++) {
+//   console.log(`7 x ${k} = ${7 * k}`);
+// }
+// let n = 12;
+// for (let m = 1; ; m++) {
+//   if (m >= n) {
+//     break;
+//   }
+//   console.log(m);
+// }
+// let x = 1;
+// while (x <= 20) {
+//   if (x % 3 === 0) {
+//     x++;
+//     continue;
+//   }
+//   console.log(x)
+//   x++
+// }
---
+++ homework8.js
@@ -0,0 +1,15 @@
+// const fruits = ['вишня','банан','яблуко','груша'];
+// let result = '';
+// for (let i = 0; i < fruits.length; i++) {
+//   result += fruits[i];
+//   if (i < fruits.length - 1) {
+//     result += ',';
+//   }
+// }
+// console.log(result);
+
+// const fruits = ['яблуко', 'банан', 'груша', 'вишня'];
+
+// const result = fruits.join(',');
+
+// console.log(result);
